29b93451430bd6e4d17197b0a55586a4b03f607c
Gideon Luck <gideonluck@hey.com>
2022-10-28 13:28:10 -0500

    removing lines

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -16,42 +16,23 @@ struct ContentView: View {
     
     let units = ["Celsius", "Farenheit", "Kelvin"]
     
-//    enum units {
-//        case Celsius
-//        case Farenheit
-//        case Kelvin
-//    }
-//
     var convertedTemp: Double {
         var newTemp = 0.0
-        
         
         if outputUnit == inputUnit {
             newTemp = inputValue
         } else if outputUnit == "Kelvin" && inputUnit == "Celsius" {
-            
             newTemp = inputValue + 273.15
-            
         } else if outputUnit == "Farenheit" && inputUnit == "Celsius" {
-            
             newTemp =  inputValue * 1.8 + 32
-            
         } else if outputUnit == "Farenheit" && inputUnit == "Kelvin" {
-            
             newTemp = (inputValue - 273.15) * 1.8 + 32
-            
         } else if outputUnit == "Celsius" && inputUnit == "Kelvin" {
-            
             newTemp = inputValue - 273.15
-            
         } else if outputUnit == "Celsius" && inputUnit == "Farenheit" {
-            
             newTemp = (inputValue - 32) / 1.8
-            
         } else if outputUnit == "Kelvin" && inputUnit == "Farenheit" {
-            
             newTemp = ((inputValue - 32) / 1.8) + 273.15
-            
         }
         
         return newTemp
