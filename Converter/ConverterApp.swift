2289a757cdeb6ffaf374b0ceb12bd33a0a5a5205
Gideon Luck <GideonLuck@hey.com>
2022-10-28 08:55:49 -0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Gideon Luck on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
